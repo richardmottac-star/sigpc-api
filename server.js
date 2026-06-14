a47de214e45b8211a8a7fa71b58411750dbe67d9
RICHARD <richardmottac@gmail.com>
2026-06-14 08:15:37 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "sigpc-api",
+  "version": "1.0.0",
+  "description": "API SIGPC-GT - Sistema de Gestão de Prestações de Contas",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "pg": "^8.11.3",
+    "cors": "^2.8.5"
+  }
+}
